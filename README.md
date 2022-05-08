67dcbf5618d9fc62e964d3e2afca3f5ecedf88ff
quanon <akshay@kivy.org>
2022-05-09 04:01:11 +0530

    Add README.md and update setup.py

---
+++ README.md
@@ -0,0 +1,11 @@
+This is a simple Python interface to lfk-mp-benchmark
+
+INSTALL
+-------
+
+Make sure to either compile the lfk-mp-benchmark library in the root of this repo or just pass the path to the library for compilation
+
+```
+python setup.py build_ext --inplace
+```
+
--- setup.py
+++ setup.py
@@ -1,55 +1,63 @@
 from distutils.core import setup, Extension
-from Cython.Build import cythonize
-import os
+import sys
+from os import environ
+from os.path import dirname, join
 
-# -----------------------------------------------------------------------------
-# Determine on which platform we are
+if environ.get('BENCHY_USE_SETUPTOOLS'):
+    from setuptools import setup, Extension
+    print('Using setuptools')
+else:
+    from distutils.core import setup
+    from distutils.extension import Extension
+    print('Using distutils')
 
-build_examples = build_examples or \
-    os.environ.get('KIVY_BUILD_EXAMPLES', '0') == '1'
+try:
+    from Cython.Distutils import build_ext
+    have_cython = True
+except ImportError:
+    have_cython = False
+
 
 platform = sys.platform
+if platform == 'win32':
+    cstdarg = '-std=gnu99'
+else:
+    cstdarg = '-std=c99'
 
-# Detect Python for android project (http://github.com/kivy/python-for-android)
-ndkplatform = environ.get('NDKPLATFORM')
-if ndkplatform is not None and environ.get('LIBLINK'):
-    platform = 'android'
-kivy_ios_root = environ.get('KIVYIOSROOT', None)
-if kivy_ios_root is not None:
-    platform = 'ios'
-# proprietary broadcom video core drivers
-if exists('/opt/vc/include/bcm_host.h'):
-    used_pi_version = pi_version
-    # Force detected Raspberry Pi version for cross-builds, if needed
-    if 'KIVY_RPI_VERSION' in environ:
-        used_pi_version = int(environ['KIVY_RPI_VERSION'])
-    # The proprietary broadcom video core drivers are not available on the
-    # Raspberry Pi 4
-    if (used_pi_version or 4) < 4:
-        platform = 'rpi'
-# use mesa video core drivers
-if environ.get('VIDEOCOREMESA', None) == '1':
-    platform = 'vc'
-mali_paths = (
-    '/usr/lib/arm-linux-gnueabihf/libMali.so',
-    '/usr/lib/arm-linux-gnueabihf/mali-egl/libmali.so',
-    '/usr/local/mali-egl/libmali.so')
-if any((exists(path) for path in mali_paths)):
-    platform = 'mali'
+if have_cython:
+    cymunk_files = ['benchy.pyx']
+    cmdclass = {'build_ext': build_ext}
+else:
+    benchy_files = ['benchy.c']
+    cmdclass = {}
 
-eo ={
-    'darwin': "lfk-mp-benchmark/build_local/cmake_build/lfk_benchmark/liblfk-benchmark.dylib",
-    'ios': 'lfk-mp-benchmark/build_local/cmake_build/lfk_benchmark/liblfk-benchmark.a',
-    'windows': 'lfk-mp-benchmark/build_local/cmake_build/lfk_benchmark/liblfk-benchmark.dll',
-    'linux': 'lfk-mp-benchmark/build_local/cmake_build/lfk_benchmark/liblfk-benchmark.so'
-}[platform]
-
-setup(ext_modules = cythonize(Extension(
-    "benchy",
+ext = Extension('benchy',
+    benchy_files,
     sources=["benchy.pyx"],
     include_dirs=['lfk-mp-benchmark/lfk_benchmark/inc'],
     language="c",
-    # extra_link_args=["-L/Users/quanon/code/waverian/benchmarkapp/benchmarkapp/service/bench/"],
-    extra_objects=eo,
-    # libraries=["liblfk-benchmark.dylib"]
-)))
+    extra_link_args=["-Llfk-mp-benchmark/build_local/cmake_build/lfk_benchmark/"],
+    libraries=["lfk-benchmark"])
+
+setup(
+    name='benchy',
+    description='Python Interface to lfk-mp-benchmark',
+    author='quanon',
+    author_email='akshay@kivy.org',
+    cmdclass=cmdclass,
+    packages=['benchy'],
+    package_data={'benchy': ['lfk-mp-benchmark/lfk_benchmark/inc/lfk.h',]},
+    package_dir={'benchy': 'benchy'},
+    ext_modules=[ext],
+    version='0.0.0.dev0'
+)
+
+
+# setup(ext_modules = cythonize(Extension(
+#     "benchy",
+#     sources=["benchy.pyx"],
+#     include_dirs=['lfk-mp-benchmark/lfk_benchmark/inc'],
+#     language="c",
+#     extra_link_args=["-Llfk-mp-benchmark/build_local/cmake_build/lfk_benchmark/"],
+#     libraries=["lfk-benchmark"]
+# )))
