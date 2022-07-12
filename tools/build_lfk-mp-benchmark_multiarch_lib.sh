7a66610aabf4239eb71f27dfc4dc854477663712
quanon <akshay@kivy.org>
2022-07-12 19:27:02 +0530

    Update Submodule & Specify --config Release(Needed for Windows)

--- tools/build_lfk-mp-benchmark_multiarch_lib.sh
+++ tools/build_lfk-mp-benchmark_multiarch_lib.sh
@@ -27,5 +27,5 @@
 mkdir -p lfk-mp-benchmark/build_local/cmake_build
 pushd lfk-mp-benchmark/build_local/cmake_build
 CMAKE_OSX_ARCHITECTURES="arm64;x86_64" cmake ../../ -DCMAKE_BUILD_TYPE=Release
-cmake --build .
+cmake --build . --config Release
 popd
