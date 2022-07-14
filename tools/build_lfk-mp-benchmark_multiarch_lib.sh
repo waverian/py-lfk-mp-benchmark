2f178c3133c4a1b73841762fe1a257b5553891e3
Kostiantyn Syrykh <cs.this@gmail.com>
2022-07-14 11:05:09 +0300

    Update lfk-mp-benchmark

--- tools/build_lfk-mp-benchmark_multiarch_lib.sh
+++ tools/build_lfk-mp-benchmark_multiarch_lib.sh
@@ -20,6 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+set -euo pipefail
+
 # let's build lfk-mp-benchmark
 
 # check if lfk-mp-benchmark has repository
